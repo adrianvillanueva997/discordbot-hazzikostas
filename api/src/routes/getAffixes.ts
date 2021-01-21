645426d5c7472f2147d82f5132c3429d4546c940
xiao <adrianvillanueva997@gmail.com>
2021-01-21 12:38:26 +0100

    Started to integrate the bot with the api

---
+++ api/src/routes/getAffixes.ts
@@ -0,0 +1,20 @@
+import express, { Request, Response } from "express";
+import { body, validationResult } from "express-validator";
+import { raider } from "../services/raider";
+
+const router = express.Router();
+
+router.get(
+  "/api/getaffixes",
+  body("region").isString(),
+  body("locale").isString(),
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { region, locale } = req.body;
+    const affixesData = await raider.getAffixes(region, locale);
+    res.status(200).json(affixesData);
+  }
+);
--- api/src/services/raider.ts
+++ api/src/services/raider.ts
@@ -1,14 +1,17 @@
 import axios from "axios";
 
 export class raider {
-    constructor() {
-    }
+  constructor() {}
 
-    async getAffixes() {
+  static async getAffixes(region: string, locale: string): Promise<any> {
+    return await axios
+      .get(
+        `https://raider.io/api/v1/mythic-plus/affixes&region=${region}&locale=${locale}`
+      )
+      .then((r) => {
+        console.log(r.data);
+      });
+  }
 
-    }
-
-    async getCharacterData() {
-
-    }
+  async getCharacterData() {}
 }
